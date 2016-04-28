43bec4af4c1910e9b6cae47f929fa085646a4eec
mikejeffers <mike.s.jeffers@gmail.com>
2016-04-27 21:28:38 -0400

    cycles images

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,4 +4,5 @@ import views
 
 urlpatterns = [
     url(r'^$', views.home, name='main'),
+    url(r'^/get-image/', views.getImage, name='get-image'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 import boto3
+from botocore.client import Config
 import random
 import project.settings as settings
 
@@ -11,22 +13,23 @@ import project.settings as settings
 
 
 
-def notHome(request):
+def home(request):
     return render(request, 'main.html', {})
 
 
-def home(request):
-    s3 = boto3.session.Session(aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+def getImage(request):
+    sesh = boto3.session.Session(aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
                                aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
-    s3 = s3.resource('s3')
+    s3 = sesh.resource('s3')
+
     bucket = s3.Bucket(settings.AWS_STORAGE_BUCKET_NAME)
+
     setOfImgUrls = []
     for obj in bucket.objects.all():
         if obj.key.find("tower") != -1 and ".db" not in obj.key:
             setOfImgUrls.append(obj.key)
+            print obj.key
 
-    index = random.randint(0, len(setOfImgUrls)-1)
-
-    context = dict()
-    context['image'] = setOfImgUrls[index]
-    return render(request, 'main.html', context)
+    index = random.randint(0, len(setOfImgUrls) - 1)
+    url = "https://"+ settings.AWS_STORAGE_BUCKET_NAME +".s3.amazonaws.com/"+setOfImgUrls[index]
+    return JsonResponse({'image': url})
