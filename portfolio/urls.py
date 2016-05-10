1687fcd28e7eca3ff200bbb344c8059c9e1a07cd
mikejeffers <mike.s.jeffers@gmail.com>
2016-05-10 14:52:24 -0400

    masonry test

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,5 +4,5 @@ import views
 
 urlpatterns = [
     url(r'^$', views.home, name='main'),
-    url(r'^/get-image/', views.getImage, name='get-image'),
+    url(r'^get-image/', views.getImage, name='get-image'),
 ]
