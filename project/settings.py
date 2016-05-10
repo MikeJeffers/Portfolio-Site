b85f9d0beb60b48b1fd68246f04c348c18506161
mikejeffers <mike.s.jeffers@gmail.com>
2016-05-10 15:02:42 -0400

    jk silly typo

--- project/settings.py
+++ project/settings.py
@@ -141,6 +141,6 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET')
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN + "/" + AWS_STORAGE_BUCKET_NAME
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
