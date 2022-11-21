3837d444d6a5b7ccc30237aa476a5c710f71b7a5
mikejeffers <mike.s.jeffers@gmail.com>
2022-11-20 22:42:52 -0500

    django-storages update syntax

--- project/settings.py
+++ project/settings.py
@@ -18,7 +18,7 @@ environ.Env.read_env(os.path.join(PROJECT_ROOT, '.env'))
 SECRET_KEY = env('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Application definition
 
@@ -143,4 +143,4 @@ if DEBUG:
 else:
     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
     STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
